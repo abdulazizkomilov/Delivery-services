1e4da7c884b45a2724958e78178e2dcde3aae497
Abdulaziz Komilov <80712573+abdulazizkomilov@users.noreply.github.com>
2024-03-31 08:25:30 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,33 +1,16 @@
-# from environs import Env
-#
-# # environs kutubxonasidan foydalanish
-# env = Env()
-# env.read_env()
-# https://officialfastfood.herokuapp.com/
+from environs import Env
 
-BOT_TOKEN = '6004482994:AAETD4wbcdA1Vg8RpJAiOGdV63j980-L_bM'  # Bot toekn
-ADMINS = [874354497]  # adminlar ro'yxati
-ADDRES = 'https://misrservicebots.up.railway.app/'  # Xosting ip manzili
+env = Env()
+env.read_env()
 
-# DB_NAME = 'postgres'
-# DB_HOST = '2001101208'
-# DB_PORT = 'tbot'
-# DB_USER = 'localhost'
-# DB_PASS = '5432'
-# from environs import Env
+BOT_TOKEN = env.str("BOT_TOKEN")
+ADMINS = env.list("ADMINS")
+ADDRES = env.list("ADMINS")
+IP = env.str("ip")
 
-# # environs kutubxonasidan foydalanish
-# env = Env()
-# env.read_env()
+DB_USER = env.str("DB_USER")
+DB_PASS = env.str("DB_PASS")
+DB_NAME = env.str("DB_NAME")
+DB_HOST = env.str("DB_HOST")
+DB_PORT = env.str("DB_PORT")
 
-# # .env fayl ichidan quyidagilarni o'qiymiz
-# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-# IP = env.str("ip")  # Xosting ip manzili
-# SECRET_KEY = 'django-insecure-f(1kes19vk(7_((i#hj!cobo*2=*ouw4#mto=gqj^-zp9ja+l-'
-
-# DB_USER = env.str("DB_USER")
-# DB_PASS = env.str("DB_PASS")
-# DB_NAME = env.str("DB_NAME")
-# DB_HOST = env.str("DB_HOST")
-# DB_PASS = '5432'
